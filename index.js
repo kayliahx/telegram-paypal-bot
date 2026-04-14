607f57760c02525a4f7395d6d075aa83e965fdc5
kayliahx <kayliah.x@gmail.com>
2026-04-14 19:28:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.BOT_TOKEN;
 
 if (!token) {
-  console.error("❌ BOT_TOKEN missing");
+  console.error("BOT_TOKEN missing");
   process.exit(1);
 }
 
 const bot = new TelegramBot(token, { polling: true });
 
-console.log("✅ Bot started");
+console.log("Bot started");
 
-bot.on('message', (msg) => {
-  console.log("📩 Received:", msg.text);
-  bot.sendMessage(msg.chat.id, "🔥 Bot is working!");
+// WHEN USER TYPES /start
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  const paypalLink = "https://www.paypal.com/webapps/billing/subscriptions?plan_id=P-7XX3454641010963FNHOTJAA";
+
+  bot.sendMessage(chatId, `Subscribe here:\n${paypalLink}`);
 });
